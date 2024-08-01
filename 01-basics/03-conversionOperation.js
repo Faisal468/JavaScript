c07de4b610fa87712166e49379246979974d1bce
Muhammad Faisal <165120835+Faisal468@users.noreply.github.com>
2024-08-01 19:52:51 +0000

    Conversion done

---
+++ 01-basics/03-conversionOperation.js
@@ -0,0 +1,19 @@
+ let score = undefined
+
+// console.log(typeof score)
+
+ let valueInNumber =  Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)  
+
+boolean = true 
+
+let isLoggedIn = 1
+
+// let  booleanIsLoggedIn = boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+let someNumber = 25;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
