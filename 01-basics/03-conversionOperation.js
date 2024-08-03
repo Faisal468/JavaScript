f7bb7050c237fa26ec72c045926a6cc9c033f33c
Muhammad Faisal <165120835+Faisal468@users.noreply.github.com>
2024-08-03 10:10:01 +0000

    Operations

--- 01-basics/03-conversionOperation.js
+++ 01-basics/03-conversionOperation.js
@@ -15,5 +15,68 @@ let isLoggedIn = 1
 let someNumber = 25;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ****************OPERATIONS***********************************
+
+
+let value = 5
+let negvalue = -value
+
+// console.log(negvalue);
+
+let x = 100
+let y = x++
+
+// console.log(x , y);
+
+
+
+let str1 = 50
+let str2 = 75
+
+let str3 = str1 * str2
+// console.log(str3);
+
+
+
+// console.log(2%3);
+// console.log(2**4);
+
+let a = 25
+let b = a++
+
+// console.log(a , b);
+
+
+let p = 20 
+let q = ++p
+
+console.log(p ,q);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
